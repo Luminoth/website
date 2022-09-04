5140b6e6ec7c8d6bffd2e093c83de3a927aecffa
Shane Lillie <ignignokterr@gmail.com>
2022-09-04 12:44:52 -0700

    Purge cloudflare

--- build/deploy-ui.sh
+++ build/deploy-ui.sh
@@ -14,21 +14,6 @@ if [ -z ${UI_DISTRIBUTION} ]; then
     exit 1
 fi
 
-#if [ -z ${CF_ZONE} ]; then
-#    echo "Missing CF_ZONE, try \`CF_ZONE={zone id} deploy-ui.sh\`"
-#    exit 1
-#fi
-#
-#if [ -z ${CF_AUTH_EMAIL} ]; then
-#    echo "Missing CF_AUTH_EMAIL, try \`CF_AUTH_EMAIL={email address} deploy-ui.sh\`"
-#    exit 1
-#fi
-#
-#if [ -z ${CF_AUTH_KEY} ]; then
-#    echo "Missing CF_AUTH_KEY, try \`CF_AUTH_KEY={key} deploy-ui.sh\`"
-#    exit 1
-#fi
-
 cd ../$UIDIR
 rm -rf dist/
 
@@ -43,9 +28,5 @@ $AWS s3 cp dist/energonsoftware s3://$BUCKET --recursive
 
 echo "Invalidating cache..."
 $AWS cloudfront create-invalidation --distribution-id $UI_DISTRIBUTION --paths "/*"
-
-#echo "Purging Cloudflare cache"
-#curl -X POST https://api.cloudflare.com/client/v4/zones/$CF_ZONE/purge_cache -H "X-Auth-Email: $CF_AUTH_EMAIL" -H "X-Auth-Key: $UI_AUTH_KEY" --data '{"purge_everything": true}'
-echo "MANUAL STEP: Purge the Cloudflare cache"
 
 echo "Done!"
