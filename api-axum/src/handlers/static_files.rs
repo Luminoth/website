15429095b015afaca90bbe6c909ebb3ccad627de
Shane Lillie <ignignokterr@gmail.com>
2024-08-14 12:38:35 -0700

    Static files routes work

---
+++ api-axum/src/handlers/static_files.rs
@@ -0,0 +1,33 @@
+use axum::{
+    debug_handler,
+    extract::{Path, State},
+    Json,
+};
+use serde::Serialize;
+
+use crate::error::AppError;
+use crate::state::AppState;
+
+#[derive(Debug, Serialize)]
+pub struct GetStaticFilesResponse {}
+
+#[debug_handler]
+pub async fn get_static_files_handler(
+    State(_app_state): State<AppState>,
+) -> Result<Json<GetStaticFilesResponse>, AppError> {
+    // TODO: read the file / dir or whatever list from S3 and return that
+    // if not prod tho, do that but using the local static files
+
+    Ok(Json(GetStaticFilesResponse {}))
+}
+
+#[debug_handler]
+pub async fn get_static_file_handler(
+    Path(_path): Path<String>,
+    State(_app_state): State<AppState>,
+) -> Result<(), AppError> {
+    // TODO: this should only be available when not prod
+    // and should just return the static file
+
+    Ok(())
+}
--- api-axum/src/routes/static_files.rs
+++ api-axum/src/routes/static_files.rs
@@ -1,18 +1,9 @@
-use axum::{debug_handler, extract::State, http::StatusCode, routing::get, Json, Router};
-use tracing::info;
+use axum::{routing::get, Router};
 
-use crate::error::AppError;
+use crate::handlers::static_files::*;
 use crate::state::AppState;
 
 pub fn init_routes(app: Router<AppState>) -> Router<AppState> {
-    app.route("/static_files", get(static_files))
-}
-
-#[debug_handler]
-async fn static_files(
-    State(app_state): State<AppState>,
-) -> Result<(StatusCode, Json<()>), AppError> {
-    info!("static files");
-
-    Ok((StatusCode::OK, Json(())))
+    app.route("/static", get(get_static_files_handler))
+        .route("/static/:path", get(get_static_file_handler))
 }
